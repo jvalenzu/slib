2d53be8ecb0595529c82094fa1673f831f9bc0f7
Joe Valenzuela <jvalenzu@infinite-monkeys.org>
2018-09-03 09:19:06 -0700

    minor formatting change
    add function separator comment block

--- Container/Util.h
+++ Container/Util.h
@@ -3,19 +3,21 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-
+// -------------------------------------------------------------------------------------------------
 template <typename ContainerType> size_t RandomIndex(ContainerType* container)
 {
   const size_t count = (1+container->Count()) * (rand() / (1.0f * RAND_MAX));
   return count;
 }
 
+// -------------------------------------------------------------------------------------------------
 template <typename ContainerType> size_t RandomIndex(const ContainerType& container)
 {
   const size_t count = (1+container.Count()) * (rand() / (1.0f * RAND_MAX));
   return count;
 }
 
+// -------------------------------------------------------------------------------------------------
 template <typename ContainerType>
 typename ContainerType::Type RandomChoicePop(ContainerType* container)
 {
@@ -25,6 +27,7 @@ typename ContainerType::Type RandomChoicePop(ContainerType* container)
   return ret;
 }
 
+// -------------------------------------------------------------------------------------------------
 template <typename ContainerType>
 typename ContainerType::Type RandomChoice(ContainerType* container)
 {
@@ -32,6 +35,7 @@ typename ContainerType::Type RandomChoice(ContainerType* container)
   return container->operator[](count);
 }
 
+// -------------------------------------------------------------------------------------------------
 template <typename ContainerType>
 typename ContainerType::Type RandomChoice(const ContainerType& container)
 {
